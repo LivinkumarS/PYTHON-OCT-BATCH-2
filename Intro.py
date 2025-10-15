5e65d852062af2d669335e8ceab18e8e163ab97f
LivinkumarS <001liv100@ggmail.com>
2025-10-15 13:34:44 +0530

    Introduction

---
+++ Intro.py
@@ -0,0 +1,11 @@
+'''These are print statements. used to print something at the output.
+These are print statements. used to print something at the output.
+These are print statements. used to print something at the output.
+These are print statements. used to print something at the output.
+These are print statements. used to print something at the output.'''
+print(12)
+print(21)
+# print(210)
+
+a=10
+print('a')
