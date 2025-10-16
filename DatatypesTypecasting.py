242da69510029f42815a14cb77ededa358b14df5
LivinkumarS <001liv100@ggmail.com>
2025-10-16 13:26:40 +0530

    Datatypes and Type Casting

---
+++ DatatypesTypecasting.py
@@ -0,0 +1,51 @@
+# a=True
+# print(a)
+
+# _a1=12
+# print(_a1)
+
+# print=10
+
+# a=-3.14
+
+# str1 = "I'm from india"
+# str2='his name is "selva"'
+# print(str2)
+
+# I'm from india
+# his name is "selva"
+
+# str1 = "Vijay"
+# str2= "Kumar"
+
+# print(str1+" "+str2+" V")
+
+name="ajith"
+movie="GBU"
+age=54
+
+# print("his name is " +name+". he has acted in "+movie)
+# print("his name is "+name+". he is "+str(age)+" years old")
+# print(f"his name is {name}. He is {age} years old.")
+
+# his name is ajith. He is 54 years old.
+
+# print(type(name)) #<class "str">
+# print(type(age)) #<class "int">
+
+# print(10)
+
+# name=input("Enter your name: ")
+
+# print("Hello "+name+"!")
+
+# str1+str2---> concatination
+
+# num1+num2 --> addition
+
+num=int(input("Enter a number: ")) #"45"
+
+print(num+60)
+
+# print(num)
+# print(type(num))
