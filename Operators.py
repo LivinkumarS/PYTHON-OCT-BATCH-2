ed05b3f3d1e01bdadf0d3d16b55f91688aa5dd7c
LivinkumarS <001liv100@ggmail.com>
2025-10-17 13:55:04 +0530

    Operators - Arithmetic, Logical, Comparison

---
+++ Operators.py
@@ -0,0 +1,43 @@
+# Arithmetic
+# a=2
+# b=0
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b)
+# print(a%b)
+# print(a**b)
+
+# print(a*b)
+# print(a**0)
+
+# print(2+3-30+16/4*2)
+
+# Comparison
+
+# a=3
+# b=30
+# print(a==b)
+# print(a!=b)
+# print(a<b)
+# print(a>b)
+# print(a<=b)
+# print(a>=b)
+
+#Assignment
+# a=10
+# a=a+a+20
+# print(a)
+
+a=100
+a+=10 #a=a+10 -->20
+a-=20 #a=a-20 -->0
+a*=4
+a/=10
+a%=3
+a//=4
+a**2
+
+print(a)
