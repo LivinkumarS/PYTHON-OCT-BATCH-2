b4e869df76554d713ac4732cc95b6c35e4a6fb64
LivinkumarS <001liv100@ggmail.com>
2025-10-23 11:24:57 +0530

    Control flow --> Conditional statements(if, elif, else)

---
+++ ControlFlow.py
@@ -0,0 +1,80 @@
+# age=int(input("Enter your age: "))
+
+# if age>=18:
+#     print("can vote")
+# else:
+#     print("can't vote")
+
+# a=10
+
+# if a<=10 and a>70: #True --> if(execute) else(ignore), False --> if(ignore) else(execute)
+#     print("From If Block")
+# else:
+#     print("From Else block")
+
+# print("Final step!")
+
+
+# if a>10: #false
+#     print("From If block!")
+# elif a<10: #false
+#     print("From Elif1 Block")
+# elif a!=10: #false
+#     print("From Elif2 Block")
+# else:
+#     print("From Else Block!")
+
+# mark
+#91-100 --> O
+#81-90 -->A+
+#71-80 -->A
+#61-70 -->B
+#50-60 -->C
+#0-49 --> Fail
+#Invalid marks
+
+# mark=int(input("Enter the mark: "))
+
+# if mark>90:
+#  	print("You're grade is 'O'")
+# elif mark>80:
+#  	print("You're grade is 'A+'")
+# elif mark>70:
+# 	print("You're grade is 'A'")
+# elif mark>60:
+# 	print("You're grade is 'B'")
+# elif mark>=50:
+# 	print("You're grade is 'C'")
+# else:
+# 	print("You failed the exam")
+
+
+# mark = int(input("Enter your marks: "))
+
+# if mark >= 91 and mark <= 100:
+#     print("Grade: O")
+# elif mark >= 81 and mark <= 90:
+#     print("Grade: A+")
+# elif mark >= 71 and mark <= 80:
+#     print("Grade: A")
+# elif mark >= 61 and mark <= 70:
+#     print("Grade: B")
+# elif mark >= 50 and mark <= 60:
+#     print("Grade: C")
+# elif mark >= 0 and mark < 50:
+#     print("Grade: Fail")
+# else:
+#     print("Invalid marks! Please enter between 0 to 100.")
+
+a=17
+
+if a<10:
+    if a<5:
+        print("very small")
+    else:
+        print("small")
+else:
+    if a>15:
+        print("very large")
+    else:
+        print("large")
