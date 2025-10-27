4c11067c3ebfe4774835fd26ea9a9521be044bf1
LivinkumarS <001liv100@ggmail.com>
2025-10-27 10:58:30 +0530

    Data structures --> List, Tuple, Set

---
+++ DataStructures.py
@@ -0,0 +1,66 @@
+# List
+# a=[1,2,3,'four','five','six',True,False,1,1,1,1]
+
+# a[5]=5
+
+# a.append(100)
+# a.extend([1,2,3,4,5])
+# a.insert(0,100)
+# a.remove(1)
+# a.remove(1)
+# a.pop(6)
+# a.pop()
+
+# print(a.index(1))
+# print(a.count(5))
+
+
+
+# lis1=[34,54,32,5,65,9,56,12,32,54,78,98,56,45445,3]
+# lis1.sort()
+# lis1.reverse()
+# print(lis1)
+
+# Tuple 
+
+# tup1=(1,2,3,4,5,6,3,32,5,3,3,2)
+# tup1[3]=100
+# print(tup1[3])
+
+# person=('KP Karuppu',28)
+# name,age=person
+
+# print(name)
+# print(age)
+
+# Set
+
+set1={1,0,0,0,2,3,4,5,6,5,4,3,2,1}
+set2={7,32,45,23,65,322,34,3,5,1}
+# set1.add(100)
+# set1.add(100)
+# set1.remove(0)
+# print(set1)
+
+# print(set1.intersection(set2))
+# print(set1.union(set2))
+# print(set1.difference(set2))
+# print(set2.difference(set1))
+
+list1=[
+    1,
+    2,
+    3,
+    [
+        'four',
+        'five',
+        'six',
+        [
+            'seven',
+            'eight',
+            'nine'
+        ]
+    ]
+]
+
+# print(list1[3][3][2])
