1cd923cd72211170896c87d9df50cd69180f4d71
LivinkumarS <001liv100@ggmail.com>
2025-10-28 10:50:12 +0530

    Data Structures - Dict

--- DataStructures.py
+++ DataStructures.py
@@ -47,20 +47,65 @@ set2={7,32,45,23,65,322,34,3,5,1}
 # print(set1.difference(set2))
 # print(set2.difference(set1))
 
-list1=[
-    1,
-    2,
-    3,
-    [
-        'four',
-        'five',
-        'six',
-        [
-            'seven',
-            'eight',
-            'nine'
-        ]
-    ]
-]
+# list1=[
+#     1,
+#     2,
+#     3,
+#     [
+#         'four',
+#         'five',
+#         'six',
+#         [
+#             'seven',
+#             'eight',
+#             'nine'
+#         ]
+#     ]
+# ]
 
 # print(list1[3][3][2])
+
+# print(list1[3][3][2][3])
+
+
+# Dict
+
+person1={
+    "name":"vijay",
+    "age":51,
+    "noOfMovies":68,
+    "isMarried":True,
+    68:"GOAT"
+}
+
+
+# print(person1["isMarried"])
+
+# person1["noOfMovies"]=69
+# print(person1)
+
+# person1[67]="Leo"
+# print(person1)
+
+# print(person1.keys())
+# print(person1.values())
+# print(person1.items())
+
+# set1={1,2,3,4,5,6}
+# lis=list(set1)
+
+# print(lis)
+
+# list1=[23,23,23,43,54,34,21,43,5,34,23,32]
+# re_dup=list(set(list1))
+
+# print(re_dup)
+# a=[324,4,32,54,67,78,65,45,43]
+# print(a[-2])
+
+# a="Hello world"
+# print(a[7:])
+# print(a[:7]) #-->0-6
+
+# print(a[2:7])
+# print(a[::-1])
